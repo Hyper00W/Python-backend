e3cb6e128a322390ef65f67055b3fa6b3053fc0a
Hyper00W <aadimanhas007@gmail.com>
2026-06-09 14:45:13 +0530

    Updated Commit

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
@@ -11,6 +20,6 @@ def get_posts():
     return {"data" : "These are your posts"}
 
 @app.post("/createpost")
-def create_post(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"New Post": f"Title: {payLoad['title']} content: {payLoad['content']}"}
+def create_post(new_post: Post):
+    print(new_post.dict())
+    return {"data": new_post}
