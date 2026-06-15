410be8cc0c72f7878cc082f19fe3c19f735939b5
Hyper00W <aadimanhas007@gmail.com>
2026-06-15 15:32:19 +0530

    refactor: restructure project with modular architecture

--- app/database.py
+++ app/database.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Aadi9654@@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Aadi9654%40@localhost:5432/fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI, Response , status , HTTPException
+from fastapi import FastAPI, Response , status , HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
 from psycopg2.extras import RealDictCursor
 import time
 
+models.Base.metadata.create_all(bind = engine)
 app = FastAPI()
 
 class Post(BaseModel):
@@ -43,18 +47,24 @@ def find_index(id):
 def read_root():
     return {"Hello":"World"}
 
+@app.get("/sqlalchemy")
+def test_post(db: Session = Depends(get_db)):
+    return {"status" : "Success"}
+
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    post = cursor.fetchall()
-    return {"data" : post}
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # post = cursor.fetchall()
+    posts = db.query(models.Post).all()
+    return {"data" : posts}
 
 @app.post("/createpost", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(title=post.title, content = post.content , published = post.published)
     return {"data " :new_post}
 
 @app.get("/posts/latest")
